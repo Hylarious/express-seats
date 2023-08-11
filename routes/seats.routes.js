ef1501fe26e5cdaf3be13cd05be76a87e425dc91
Karolina Hyla <karatakarabe@gmail.com>
2023-08-11 21:53:19 +0200

    Add festival app wit express

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -9,27 +9,28 @@ router.route('/seats').get((req, res) => {
 });
 
 router.route('/seats/:id').get((req, res) => {
-if (req.params.id == 'random') {
-    const random = Math.floor(Math.random() * db.seats.length) 
-    res.json(db.seats[random])
-}
-else { res.send(db.seats.find(seat => seat.id == req.params.id))}
+    res.send(db.seats.find(seat => seat.id == req.params.id))
 });
 
 router.route('/seats').post((req, res) => {
-const { day, seat, client, email } = req.body
-const newTestimonial = { id: randomID(5), day, seat, client, email };
-db.seats.push(newTestimonial);
-res.json(db.seats)
+    const { day, seat, client, email } = req.body
+    if(db.seats.some(item => item.seat == seat && item.day == day)) {
+        res.status(409).json({ message: "The slot is already taken..." }
+)
+    } else {
+        const newTestimonial = { id: randomID(5), day, seat, client, email };
+        db.seats.push(newTestimonial);
+        res.json(db.seats)
+    }
 })
 
 router.route('/seats/:id').put((req, res) => {
-const { day, seat, client, email } = req.body
-res.json(db.seats.map(seat => (seat.id == req.params.id ? { id:seat.id, day, seat, client, email} : seat)))
+    const { day, seat, client, email } = req.body
+    res.json(db.seats.map(s => (s.id == req.params.id ? { id:s.id, day, seat, client, email} : s)))
 })
 
 router.route('/seats/:id').delete((req, res) => {
-res.json(db.seats.filter(seat => seat.id != req.params.id))
+    res.json(db.seats.filter(seat => seat.id != req.params.id))
 })
 
 module.exports = router;
--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -7,29 +7,29 @@ router.route('/testimonials').get((req, res) => {
     console.log(db)
     res.json(db.testimonials)
 });
-
-router.route('/testimonials/:id').get((req, res) => {
-if (req.params.id == 'random') {
+router.route('/testimonials/random').get((req, res) => {
     const random = Math.floor(Math.random() * db.testimonials.length) 
     res.json(db.testimonials[random])
-}
-else { res.send(db.testimonials.find(testimony => testimony.id == req.params.id))}
+})
+
+router.route('/testimonials/:id').get((req, res) => {
+    res.send(db.testimonials.find(testimony => testimony.id == req.params.id))
 });
 
 router.route('/testimonials').post((req, res) => {
-const { author, text } = req.body
-const newTestimonial = { id: randomID(5), author, text };
-db.testimonials.push(newTestimonial);
-res.json(db.testimonials)
+    const { author, text } = req.body
+    const newTestimonial = { id: randomID(5), author, text };
+    db.testimonials.push(newTestimonial);
+    res.json(db.testimonials)
 })
 
 router.route('/testimonials/:id').put((req, res) => {
-const { author, text } = req.body
-res.json(db.testimonials.map(testimony => (testimony.id == req.params.id ? { id:testimony.id, author, text} : testimony)))
+    const { author, text } = req.body
+    res.json(db.testimonials.map(testimony => (testimony.id == req.params.id ? { id:testimony.id, author, text} : testimony)))
 })
 
 router.route('/testimonials/:id').delete((req, res) => {
-res.json(db.testimonials.filter(testimony => testimony.id != req.params.id))
+    res.json(db.testimonials.filter(testimony => testimony.id != req.params.id))
 })
 
 module.exports = router;
